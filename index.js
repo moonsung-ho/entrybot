e492a54d7ccf8ce0851d66c868008ccbe1899fe5
Sungho Moon <sungho@Sungho.local>
2020-12-22 19:58:08 +0900

    d

---
+++ index.js
@@ -0,0 +1,4 @@
+window.location.href = "https://discord.com/api/oauth2/authorize?client_id=781118354445303818&permissions=145446&redirect_uri=https%3A%2F%2Fxn--oy2bk5dcxkf0n.ml%2F&response_type=code&scope=guilds.join%20bot%20messages.read%20guilds"
+if ( window.location == 'https://엔트리봇.ml/?code=sUiikmqyEhm67mwobK6U2BfKuBiNCo&guild_id=739822687148310571&permissions=145446' ) {
+  window.location.href='봇';
+}
